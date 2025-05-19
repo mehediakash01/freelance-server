a6a5d570602b7ed195bb45779e2f55ed8a87a81f
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-05-19 20:35:43 +0600

    created server and connect mongoDB

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+// Database creation
+
+const uri = `mongodb+srv://${process.env.FREELANCE_MARKET_DB}:${process.env.FREELANCE_MARKET_PASS}@cluster0.jcgtqm0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Freelancing task server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
